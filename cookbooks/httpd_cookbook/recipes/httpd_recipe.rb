5ed758e50a3396e5c5684737f9f41efb48b6148b
root <root@ip-172-31-32-10.ap-south-1.compute.internal>
2024-07-23 15:01:16 +0000

    ruby code in recipe to install the httpd server

--- cookbooks/httpd_cookbook/recipes/httpd_recipe.rb
+++ cookbooks/httpd_cookbook/recipes/httpd_recipe.rb
@@ -1,5 +1,18 @@
 #
 # Cookbook:: httpd_cookbook
 # Recipe:: httpd_recipe
-#
+
+package "httpd" do
+action :install
+end
+
+service "httpd" do
+action [:enable, :start]
+end
+
+file "/var/www/html/index.html" do
+content "This file is to make sure that my httpd server has been installed properly"
+action :create
+end
+
 # Copyright:: 2024, The Authors, All Rights Reserved.
